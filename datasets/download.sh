f159ade0849ad60a2f114beb992e154a4e798013
ramesht007 <shre611shukla@gmail.com>
2022-04-04 03:16:50 +0000

    add stackoverflow data and citation for fedscale

--- datasets/download.sh
+++ datasets/download.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Thanks to FedScale provides a large suite of FL datasets to evaluate today's FL performance.
+# https://github.com/SymbioticLab/FedScale/blob/master/dataset/download.sh
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
@@ -15,6 +17,7 @@ sed -i "s/datafile.*$/datafile : \"${PATH_FORMAT}\"/" ../configs/${Model}.yml
 Help()
 {
     echo "-f    Download femnist dataset for Federated Learning Experiments"
+    echo "-o    Download StackOverflow dataset (about 13G)"
 }
 
 femnist()
@@ -35,14 +38,37 @@ femnist()
         echo -e "${RED}femnist dataset already exists under ${DIR}/femnist/!"
 fi
 }
+
+stackoverflow() 
+{
+    if [ ! -d "${DIR}/stackoverflow/train/" ]; 
+    then
+        echo "Downloading stackoverflow dataset(about 800M)..."   
+        wget -O ${DIR}/stackoverflow.tar.gz https://fedscale.eecs.umich.edu/dataset/stackoverflow.tar.gz
+        
+        echo "Dataset downloaded, now decompressing..." 
+        tar -xf ${DIR}/stackoverflow.tar.gz -C ${DIR}
+
+        echo "Removing compressed file..."
+        rm -f ${DIR}/stackoverflow.tar.gz
+
+        echo -e "${GREEN}stackoverflow dataset downloaded!${NC}"
+    else
+        echo -e "${RED}stackoverflow dataset already exists under ${DIR}/stackoverflow/!"
+fi
+}
+
 while getopts ":hsoacegildrtwfxo" option; do
    case $option in
       h ) # display Help
          Help
          exit;;
-      f )
+      f ) # identifier for femnist dataset
          femnist
-         ;;           
+         ;;
+      o ) # identifier for stackoverflow dataset
+         stackoverflow
+         ;;             
       \? ) 
          echo -e "${RED}Usage: cmd [-h] [-A] [-o] [-t] [-p]${NC}"
          exit 1;;
